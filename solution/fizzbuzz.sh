733211f5e00d19143fb4598c17691f0e784b65d8
SherwoodXL <sherwoodgl@HOME-PC.localdomain>
2025-10-01 13:33:48 +0700

    Update 1.5

--- solution/fizzbuzz.sh
+++ solution/fizzbuzz.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-n="$@"
+n="$*"
 
 for ((i=1; i<=n; i++)); do
-	if [ $((i % 3)) -eq 0 -a $((i % 5)) -eq 0 ]; then
+	if [[ $((i % 3)) -eq 0&& $((i % 5)) -eq 0 ]]; then
 		echo "Fizz Buzz"
 	elif [ $((i % 3)) -eq 0 ]; then
 		echo "Fizz"
